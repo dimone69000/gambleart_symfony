e07099c752e115ab1e6af0279d2605446981aca5
dimone69000 <dimitri-bonnet@hotmail.fr>
2023-11-25 09:49:34 +0100

    creation des entites hors user, generation des fichiers de migrations

---
+++ src/Entity/Nft.php
@@ -0,0 +1,81 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\NftRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: NftRepository::class)]
+class Nft
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $imageUrl = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $dateDrop = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImageUrl(): ?string
+    {
+        return $this->imageUrl;
+    }
+
+    public function setImageUrl(string $imageUrl): static
+    {
+        $this->imageUrl = $imageUrl;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getDateDrop(): ?\DateTimeInterface
+    {
+        return $this->dateDrop;
+    }
+
+    public function setDateDrop(\DateTimeInterface $dateDrop): static
+    {
+        $this->dateDrop = $dateDrop;
+
+        return $this;
+    }
+}
